e2bee4dad6b77bd14f5fe78d3b0c7ca9c4cafb24
Deepak kumar <deepakkumardivakar76@gmail.com>
2021-08-19 00:24:09 +0530

    day 2 assignment

---
+++ calc.js
@@ -0,0 +1,10 @@
+     
+        function addtolist() {
+            var v1=document.getElementById("number");
+            var v2=document.getElementById("numberList");
+             v2=v1.value;
+            
+        }
+    
+   
+  
