8d275701fabf5043e58a357fa1dcbbed42eb2124
Deepak kumar <deepakkumardivakar76@gmail.com>
2021-08-26 09:24:21 +0530

    day7 assignment

---
+++ assignment7.2.js
@@ -0,0 +1,26 @@
+const { rejects } = require("assert/strict");
+const { resolve } = require("path/posix");
+const { resourceLimits } = require("worker_threads");
+
+function countDown(maxval){
+    return new Promise((resolve,rejects)=>{
+        let ans=[];
+        let tid=setInterval(()=>{
+            for(let i=maxval;i>=0;i--)
+              ans.push(i);
+
+            return ;
+        },1000);
+        if(maxval<0){
+            return rejects(new Error("value you have enter less than 0"));
+
+        }else{
+            return resolve(ans);
+        }
+    });
+}
+countDown(10).then(ans=>{
+    console.log(ans);
+}).catch(error=>{
+    console.log(error);
+});
